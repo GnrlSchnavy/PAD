fa7624168bb004397a702b5550462a2c6745c3ed
GnrlSchnavy <yvanstemmerik@hotmail.com>
2015-06-17 23:04:05 +0200

    A3 done

--- src/A4/CompleteLinkage.java
+++ src/A4/CompleteLinkage.java
@@ -1,7 +1,28 @@
-package A4;
+import A4.DistanceMeasure;
 
-/**
- * Created by Yvan on 17-6-2015.
- */
-public class CompleteLinkage {
+public class CompleteLinkage implements ClusterMethod {
+	
+	/* Variable declaration(s) */
+
+
+	/* Constructor(s) */
+
+    CompleteLinkage(DistanceMeasure distanceMeasure) {
+        this.distanceMeasure = distanceMeasure;
+    }
+	
+	/* Interface methods */
+
+    public double calculateDistance(Cluster cluster1, Cluster cluster2) {
+
+        double thisDistance;
+        double smallestDistance = Double.MAX_VALUE;
+        for (int c1 = 0; c1 < cluster1.getWidth(); c1++){
+            for (int c2 = 0; c2 < cluster2.getWidth(); c2++){
+                thisDistance = distanceMeasure.calculateDistance(cluster1.getUnit(c1), cluster2.getUnit(c2));
+                if (thisDistance < smallestDistance) smallestDistance = thisDistance;
+            }
+        }
+        return smallestDistance;
+    }
 }
